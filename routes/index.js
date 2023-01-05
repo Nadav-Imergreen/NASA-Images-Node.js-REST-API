2490c522aa2a05ba17a6022ac066c41a51e5f1d3
moshe <moshenam@edu.hac.ac.il>
2023-01-06 01:02:28 +0200

    icons and button

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' , imglogo: '/images/0.JPG'});
+  res.render('index', { title: 'Express' , imglogo: '/images/0.png'});
 });
 
 module.exports = router;
