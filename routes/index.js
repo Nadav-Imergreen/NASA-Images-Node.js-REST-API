bae6afcf9691b815c03cac5054cb9c760720fc04
moshe <moshenam@edu.hac.ac.il>
2023-01-06 15:52:46 +0200

    register and password pages

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' , imglogo: '/images/0.png'});
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Express' , imglogo: '/images/0.png'});
+});
+
+router.get('/register-password', function(req, res, next) {
+  res.render('register-password', { title: 'Express' , imglogo: '/images/0.png'});
+});
+
 module.exports = router;
