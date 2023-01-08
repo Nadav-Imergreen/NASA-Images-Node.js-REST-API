bb76cded499a1e4567393b9d592f88b8daea5a06
moshe <moshenam@edu.hac.ac.il>
2023-01-08 18:54:06 +0200

    emails and passwords

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var emails = [ ];
 
 
 /* GET home page. */
@@ -8,11 +9,29 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Express' , imglogo: '/images/0.png'});
+  res.render('register', { title: 'Express' , imglogo: '/images/0.png', mailExists:false});
 });
 
 router.get('/register-password', function(req, res, next) {
-  res.render('register-password', { title: 'Express' , imglogo: '/images/0.png'});
+  res.render('register-password', { title: 'Express' , imglogo: '/images/0.png', equalPassword:true});
+});
+
+router.post('/register', function(req, res, next) {
+  if(emails.includes(req.body.email))
+    res.render('register', { title: 'Express' , imglogo: '/images/0.png', mailExists:true });
+
+  else
+  {
+    emails.push(req.body.email);
+    res.render('register-password', { title: 'Express' , imglogo: '/images/0.png', mailExists:false, equalPassword:true});
+  }
+});
+
+router.post('/register-password', function(req, res, next) {
+  if(req.body.password === req.body.confirmPassword)
+    res.render('index', { title: 'Express' , imglogo: '/images/0.png', equalPassword:true});
+  else
+    res.render('register-password', { title: 'Express' , imglogo: '/images/0.png', equalPassword:false});
 });
 
 module.exports = router;
