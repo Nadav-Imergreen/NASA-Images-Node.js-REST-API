8b8dc29ce5cf3074e751d4f6893b5e0981c36862
Nadav <ne600ne@gmail.com>
2023-01-09 21:06:29 +0200

    full cookies

--- routes/index.js
+++ routes/index.js
@@ -6,47 +6,67 @@ var emails = [];
 const keys = ['keyboard cat'];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Express' , imglogo: '/images/0.png'});
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express', imgLogo: '/images/0.png'});
 });
 
-router.get('/register', function(req, res, next) {
+router.get('/register', function (req, res, next) {
     // get the cookie
-  const cookies = new Cookies(req, res, { keys: keys});
-  const email = cookies.get('email', { signed: true });
-  // load page content to browser
-  res.render('register', {title: 'Express' , imglogo: '/images/0.png', mailExists:false, email: email});
-});
-
-router.get('/register-password', function(req, res, next) {
-    res.render('register-password', {title: 'Express' , imglogo: '/images/0.png', equalPassword:true});
-});
-
-
-router.post('/register', function(req, res, next) {
-
-    const cookies = new Cookies(req, res, { keys: keys})
-    const notFirstVisit = cookies.get('email', {signed: true });
-
-    if (!notFirstVisit)
-    //set new cookie
-    cookies.set('email' ,req.body.email, {signed: true, maxAge: 30*1000 });
-
-    if(emails.includes(req.body.email))
-        res.render('register', {title: 'Express' , imglogo: '/images/0.png', mailExists: true, email: notFirstVisit});
+    const cookies = new Cookies(req, res, {keys: keys});
+    const userDetails = cookies.get('userDetails', {signed: true});
+    if (userDetails)
+    {
+        let data = JSON.parse(userDetails);
+        // load page content to browser
+        res.render('register', {title: 'Express', imgLogo: '/images/0.png', mailExists: false,
+            email: data.email,   firstName: data.firstName, lastName: data.lastName});
+    }
     else
     {
-      emails.push(req.body.email);
-      res.redirect('register-password');
+        res.render('register', {title: 'Express', imgLogo: '/images/0.png', mailExists: false,
+            email:'', firstName: '', lastName: ''});
     }
 });
 
-router.post('/register-password', function(req, res, next) {
+router.get('/register-password', function (req, res, next) {
+    res.render('register-password', {title: 'Express', imgLogo: '/images/0.png', equalPassword: true});
+});
 
-  if(req.body.password === req.body.confirmPassword)
-      res.redirect('/')
-  else
-      res.render('register-password', {title: 'Express' , imglogo: '/images/0.png', equalPassword: false});
+
+router.post('/register', function (req, res, next) {
+
+    const cookies = new Cookies(req, res, {keys: keys})
+    const userDetails = cookies.get('userDetails', {signed: true});
+
+    if (!userDetails) {
+        //set new cookie
+        let newCookie = {
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName
+        };
+        cookies.set('userDetails', JSON.stringify(newCookie), {signed: true, maxAge: 30 * 1000});
+    }
+
+    if (emails.includes(req.body.email))
+    {
+        let data = JSON.parse(userDetails);
+
+        res.render('register', {title: 'Express', imgLogo: '/images/0.png', mailExists: true,
+            email: data.email,   firstName: data.firstName, lastName: data.lastName});
+    }
+    else {
+        emails.push(req.body.email);
+        res.redirect('register-password');
+    }
+});
+
+router.post('/register-password', function (req, res, next) {
+
+    if (req.body.password === req.body.confirmPassword)
+        res.redirect('/')
+    else
+        res.render('register-password', {title: 'Express', imgLogo: '/images/0.png', equalPassword: false});
 });
 
 module.exports = router;
