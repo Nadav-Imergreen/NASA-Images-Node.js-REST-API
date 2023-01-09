554b31a519691bd9a9440a10e7d992e8084c2e49
Nadav <ne600ne@gmail.com>
2023-01-09 19:42:04 +0200

    cookies&redirect

--- routes/index.js
+++ routes/index.js
@@ -11,34 +11,33 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/register', function(req, res, next) {
-
+    // get the cookie
   const cookies = new Cookies(req, res, { keys: keys});
-  // get the cookie
-  const notFirstVisit = cookies.get('email', { signed: true });
-    if (notFirstVisit)
-        res.render('register', {title: 'Express' , imglogo: '/images/0.png', mailExists:false, email: notFirstVisit});
-    else
-        res.render('register', {title: 'Express' , imglogo: '/images/0.png', mailExists:false, email: ''});
+  const email = cookies.get('email', { signed: true });
+  // load page content to browser
+  res.render('register', {title: 'Express' , imglogo: '/images/0.png', mailExists:false, email: email});
 });
 
-router.post('/register/password', function(req, res, next) {
+router.get('/register-password', function(req, res, next) {
+    res.render('register-password', {title: 'Express' , imglogo: '/images/0.png', equalPassword:true});
+});
+
+
+router.post('/register', function(req, res, next) {
 
     const cookies = new Cookies(req, res, { keys: keys})
-    const notFirstVisit = cookies.get('email', { signed: true });
+    const notFirstVisit = cookies.get('email', {signed: true });
 
     if (!notFirstVisit)
     //set new cookie
-    cookies.set('email' ,req.body.email, { signed: true, maxAge: 30*1000 });
+    cookies.set('email' ,req.body.email, {signed: true, maxAge: 30*1000 });
 
     if(emails.includes(req.body.email))
-    {
-        //res.redirect('register')
-        res.render('register', {title: 'Express' , imglogo: '/images/0.png', mailExists:true, email: notFirstVisit});
-    }
+        res.render('register', {title: 'Express' , imglogo: '/images/0.png', mailExists: true, email: notFirstVisit});
     else
     {
       emails.push(req.body.email);
-      res.render('register-password', {title: 'Express' , imglogo: '/images/0.png', mailExists:false, equalPassword:true});
+      res.redirect('register-password');
     }
 });
 
@@ -46,12 +45,8 @@ router.post('/register-password', function(req, res, next) {
 
   if(req.body.password === req.body.confirmPassword)
       res.redirect('/')
-    //res.render('index', {title: 'Express' , imglogo: '/images/0.png', equalPassword:true});
   else
-  {
-      res.render('register-password', {title: 'Express' , imglogo: '/images/0.png', equalPassword:false});
-  }
-
+      res.render('register-password', {title: 'Express' , imglogo: '/images/0.png', equalPassword: false});
 });
 
 module.exports = router;
