b409d5f3799848fa947764650d67519761fa1733
Nadav <ne600ne@gmail.com>
2023-01-10 17:33:31 +0200

    with working refresh

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const keys = ['keyboard cat'];
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', {title: 'Express', imgLogo: '/images/0.png', RegistrationSucceeded:false});
+    res.render('index', {title: 'Express', imgLogo: '/images/0.png', RegistrationSucceeded: false});
 });
 
 router.get('/register/:cookieTime', function (req, res) {
@@ -17,9 +17,15 @@ router.get('/register/:cookieTime', function (req, res) {
     let userDetails = cookies.get('userDetails', {signed: true});
     let refreshErrors = cookies.get('refreshErrors', {signed: true});
 
-    if (refreshErrors)
-        res.render('register', {title: 'Express', imgLogo: '/images/0.png', mailExists: true,
-            cookieExpired: false, email:'',   firstName:'', lastName:''});
+    if (refreshErrors) {
+        if (userDetails) {
+            let data = JSON.parse(userDetails);
+            res.render('register', {
+                title: 'Express', imgLogo: '/images/0.png', mailExists: true,
+                cookieExpired: false, email: data.email, firstName: data.firstName, lastName: data.lastName
+            });
+        }
+    }
 
     if (req.params.cookieTime !== 'expire') // cookie time not expire
     {
@@ -27,23 +33,27 @@ router.get('/register/:cookieTime', function (req, res) {
         {
             let data = JSON.parse(userDetails);
             // load page content to browser
-            res.render('register', {title: 'Express', imgLogo: '/images/0.png', mailExists: false,
-                cookieExpired: false, email: data.email,   firstName: data.firstName, lastName: data.lastName});
-        }
-        else // first entry
-            res.render('register', {title: 'Express', imgLogo: '/images/0.png', mailExists: false,
-                cookieExpired: false, email:'', firstName: '', lastName: ''});
-    }
-    else // cookie time expire
-        res.render('register', {title: 'Express', imgLogo: '/images/0.png', mailExists: false,
-            cookieExpired: true, email:'', firstName: '', lastName: ''});
+            res.render('register', {
+                title: 'Express', imgLogo: '/images/0.png', mailExists: false,
+                cookieExpired: false, email: data.email, firstName: data.firstName, lastName: data.lastName
+            });
+        } else // first entry
+            res.render('register', {
+                title: 'Express', imgLogo: '/images/0.png', mailExists: false,
+                cookieExpired: false, email: '', firstName: '', lastName: ''
+            });
+    } else // cookie time expire
+        res.render('register', {
+            title: 'Express', imgLogo: '/images/0.png', mailExists: false,
+            cookieExpired: true, email: '', firstName: '', lastName: ''
+        });
 
 });
 
 router.get('/register-password', function (req, res) {
 
     const cookies = new Cookies(req, res, {keys: keys})
-    if(cookies.get('userDetails', {signed: true}))
+    if (cookies.get('userDetails', {signed: true}))
         res.render('register-password', {title: 'Express', imgLogo: '/images/0.png', equalPassword: true});
     else
         res.redirect('/register/notExpire')
@@ -58,14 +68,13 @@ router.post('/register', function (req, res) {
     let newCookie = {email: req.body.email, firstName: req.body.firstName, lastName: req.body.lastName};
     cookies.set('userDetails', JSON.stringify(newCookie), {signed: true, maxAge: 10 * 1000});
 
-    if (emails.includes(req.body.email)){
+    if (emails.includes(req.body.email)) {
 
-            //set new cookie
-            cookies.set('refreshErrors', 'errorExist', {signed: true, maxAge: 1000});
+        //set new cookie
+        cookies.set('refreshErrors', 'errorExist', {signed: true, maxAge: 1000});
 
         res.redirect('/register/notExpire');
-    }
-    else
+    } else
         res.redirect('register-password');
 });
 
@@ -75,19 +84,15 @@ router.post('/register-password', function (req, res) {
     const userDetails = cookies.get('userDetails', {signed: true});
     let data;
 
-    if (userDetails)
-    {
-        if (req.body.password === req.body.confirmPassword)
-        {
+    if (userDetails) {
+        if (req.body.password === req.body.confirmPassword) {
             data = JSON.parse(userDetails);
             emails.push(data.email);
             cookies.set('userDetails', userDetails, {signed: true, maxAge: -1});
-            res.render('index', {title: 'Express', imgLogo: '/images/0.png', RegistrationSucceeded:true});
-        }
-        else
+            res.render('index', {title: 'Express', imgLogo: '/images/0.png', RegistrationSucceeded: true});
+        } else
             res.render('register-password', {title: 'Express', imgLogo: '/images/0.png', equalPassword: false});
-    }
-    else
+    } else
         res.redirect('/register/expire');
 
 });
